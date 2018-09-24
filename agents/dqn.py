eff0eb1b7d0efc024e283ec9891a30b31bdb6fa1
Jonathan <jonsecu0001@hotmail.com>
2018-09-24 16:08:47 -0500

    agente dqn refactored

---
+++ agents/dqn.py
@@ -0,0 +1,120 @@
+import numpy as np
+from collections import deque
+from networks.qnetworks import conv_network, ff_network
+import tensorflow as tf
+from summaries.summaries import variable_summaries, simple_summaries
+
+
+class DQNAgent:
+    def __init__(self, num_actions, eps_min, eps_max, eps_decay_steps):
+        # replay memory
+        self.replay_memory_size = 20000
+        self.replay_memory = deque([], maxlen=self.replay_memory_size)
+        # learning and environment
+        self.num_actions = num_actions
+        self.eps_min = eps_min
+        self.eps_max = eps_max
+        self.eps_decay_steps = eps_decay_steps
+        # training
+        self.batch_size = 50
+        self.discount_rate = 0.99
+        self.X_state = None
+        self.online_q_values = None
+        self.online_vars = None
+        self.target_q_values = None
+        self.target_vars = None
+        self.copy_ops = None
+        self.copy_online_to_target = None
+        self.learning_rate = 0.01
+        self.momentum = 0.95
+        self.X_action = None
+        self.y = None
+        self.q_value = None
+        self.error = None
+        self.clipped_error = None
+        self.linear_error = None
+        self.loss = None
+        self.global_step = tf.Variable(0, trainable=False, name='global_step')
+        self.optimizer = None
+        self.training_op = None
+        # tensorflow saver and logger
+        self.init = None
+        self.merged = None
+        self.saver = None
+
+        pass
+
+    def create_networks(self, x_state):
+
+        self.X_state = x_state
+
+        self.online_q_values, self.online_vars = ff_network(x_state, n_outputs=self.num_actions,
+                                                            name="q_networks/online")
+        self.target_q_values, self.target_vars = ff_network(x_state, n_outputs=self.num_actions,
+                                                            name="q_networks/target")
+
+        # We need an operation to copy the online DQN to the target DQN
+        self.copy_ops = [target_var.assign(self.online_vars[var_name])
+                    for var_name, target_var in self.target_vars.items()]
+        self.copy_online_to_target = tf.group(*self.copy_ops)
+
+        # Now for the training operations
+        with tf.variable_scope("train"):
+            self.X_action = tf.placeholder(tf.int32, shape=[None])
+            self.y = tf.placeholder(tf.float32, shape=[None, 1])
+            self.q_value = tf.reduce_sum(self.online_q_values * tf.one_hot(self.X_action, self.num_actions),
+                                    axis=1, keep_dims=True)
+            self.error = tf.abs(self.y - self.q_value)
+            self.clipped_error = tf.clip_by_value(self.error, 0.0, 1.0)
+            self.linear_error = 2 * (self.error - self.clipped_error)
+            self.loss = tf.reduce_mean(tf.square(self.clipped_error) + self.linear_error)
+
+            self.optimizer = tf.train.MomentumOptimizer(
+                self.learning_rate, self.momentum, use_nesterov=True)
+            self.training_op = self.optimizer.minimize(self.loss, global_step=self.global_step)
+
+        # agrupa los summaries en el grafo para que no aparezcan por todos lados
+        with tf.name_scope('summaries'):
+            simple_summaries(self.linear_error, 'linear_error')
+            simple_summaries(self.loss, 'loss')
+            simple_summaries(self.online_q_values, 'online_q_values')
+
+        # evita agregar al grafo los summaries uno por uno
+        self.merged = tf.summary.merge_all()
+
+        self.init = tf.global_variables_initializer()
+        self.saver = tf.train.Saver()
+
+    def sample_memories(self, batch_size):
+        indices = np.random.permutation(len(self.replay_memory))[:batch_size]
+        cols = [[], [], [], [], []]  # state, action, reward, next_state, continue
+        for idx in indices:
+            memory = self.replay_memory[idx]
+            for col, value in zip(cols, memory):
+                col.append(value)
+        cols = [np.array(col) for col in cols]
+        return (cols[0], cols[1], cols[2].reshape(-1, 1), cols[3],
+                cols[4].reshape(-1, 1))
+
+    def epsilon_greedy(self, q_values, step):
+        epsilon = max(self.eps_min,
+                      self.eps_max - (self.eps_max - self.eps_min) * step / self.eps_decay_steps)
+        if np.random.rand() < epsilon:
+            return np.random.randint(self.num_actions)  # random action
+        else:
+            return np.argmax(q_values)  # optimal action
+
+    def preprocess_observation(self, obs):
+        # img = obs[1:176:2, ::2]  # crop and downsize
+        # img = img.mean(axis=2)  # to greyscale
+        # img[img == mspacman_color] = 0  # Improve contrast
+        # img = (img - 128) / 128 - 1  # normalize from -1. to 1.
+        # return img.reshape(88, 80, 1)
+        return obs
+
+    def sample(self):
+        self.X_state_val, self.X_action_val, self.rewards, self.X_next_state_val, self.continues = (
+            self.sample_memories(self.batch_size))
+        self.next_q_values = self.target_q_values.eval(feed_dict={self.X_state: self.X_next_state_val})
+        self.max_next_q_values = np.max(self.next_q_values, axis=1, keepdims=True)
+        self.y_val = self.rewards + self.continues * self.discount_rate * self.max_next_q_values
--- tiny_dqn.py
+++ tiny_dqn.py
@@ -8,6 +8,7 @@ import tensorflow as tf
 from networks.qnetworks import conv_network, ff_network
 from summaries.summaries import variable_summaries, simple_summaries
 from collections import namedtuple
+from agents.dqn import DQNAgent
 
 args_struct = namedtuple(
     'args',
@@ -55,96 +56,21 @@ n_outputs = env.action_space.n  # 9 discrete actions are available
 # X_state = tf.placeholder(tf.float32, shape=[None, input_height, input_width, input_channels])
 X_state = tf.placeholder(tf.float32, shape=[None, input_height * input_width])
 
-online_q_values, online_vars = ff_network(X_state, n_outputs=n_outputs, name="q_networks/online")
-target_q_values, target_vars = ff_network(X_state, n_outputs=n_outputs, name="q_networks/target")
-
-# We need an operation to copy the online DQN to the target DQN
-copy_ops = [target_var.assign(online_vars[var_name])
-            for var_name, target_var in target_vars.items()]
-copy_online_to_target = tf.group(*copy_ops)
-
-# Now for the training operations
-learning_rate = 0.01
-momentum = 0.95
-
-with tf.variable_scope("train"):
-    X_action = tf.placeholder(tf.int32, shape=[None])
-    y = tf.placeholder(tf.float32, shape=[None, 1])
-    q_value = tf.reduce_sum(online_q_values * tf.one_hot(X_action, n_outputs),
-                            axis=1, keep_dims=True)
-    error = tf.abs(y - q_value)
-    clipped_error = tf.clip_by_value(error, 0.0, 1.0)
-    linear_error = 2 * (error - clipped_error)
-    loss = tf.reduce_mean(tf.square(clipped_error) + linear_error)
-
-    global_step = tf.Variable(0, trainable=False, name='global_step')
-    optimizer = tf.train.MomentumOptimizer(
-        learning_rate, momentum, use_nesterov=True)
-    training_op = optimizer.minimize(loss, global_step=global_step)
-
-# agrupa los summaries en el grafo para que no aparezcan por todos lados
-with tf.name_scope('summaries'):
-    simple_summaries(linear_error, 'linear_error')
-    simple_summaries(loss, 'loss')
-    simple_summaries(online_q_values, 'online_q_values')
-
-
-# evita agregar al grafo los summaries uno por uno
-merged = tf.summary.merge_all()
-
-init = tf.global_variables_initializer()
-saver = tf.train.Saver()
-
-# Let's implement a simple replay memory
-replay_memory_size = 20000
-replay_memory = deque([], maxlen=replay_memory_size)
-
-
-def sample_memories(batch_size):
-    indices = np.random.permutation(len(replay_memory))[:batch_size]
-    cols = [[], [], [], [], []]  # state, action, reward, next_state, continue
-    for idx in indices:
-        memory = replay_memory[idx]
-        for col, value in zip(cols, memory):
-            col.append(value)
-    cols = [np.array(col) for col in cols]
-    return (cols[0], cols[1], cols[2].reshape(-1, 1), cols[3],
-            cols[4].reshape(-1, 1))
-
-
 # And on to the epsilon-greedy policy with decaying epsilon
 eps_min = 0.1
 eps_max = 1.0 if not args.test else eps_min
 eps_decay_steps = args.number_steps // 2
 
-
-def epsilon_greedy(q_values, step):
-    epsilon = max(eps_min, eps_max - (eps_max - eps_min) * step / eps_decay_steps)
-    if np.random.rand() < epsilon:
-        return np.random.randint(n_outputs)  # random action
-    else:
-        return np.argmax(q_values)  # optimal action
-
-
 # We need to preprocess the images to speed up training
 mspacman_color = np.array([210, 164, 74]).mean()
 
-
-def preprocess_observation(obs):
-    # img = obs[1:176:2, ::2]  # crop and downsize
-    # img = img.mean(axis=2)  # to greyscale
-    # img[img == mspacman_color] = 0  # Improve contrast
-    # img = (img - 128) / 128 - 1  # normalize from -1. to 1.
-    # return img.reshape(88, 80, 1)
-    return obs
-
+agent = DQNAgent(n_outputs, eps_min, eps_max, eps_decay_steps)
+agent.create_networks(X_state)
 
 # TensorFlow - Execution phase
 training_start = args.training_start
-discount_rate = 0.99
 skip_start = 0  # Skip the start of every game (it's just waiting time).
 # skip_start = 90  # Skip the start of every game (it's just waiting time).
-batch_size = 50
 iteration = 0  # game iterations
 done = True  # env needs to be reset
 
@@ -156,15 +82,15 @@ mean_max_q = 0.0
 
 with tf.Session() as sess:
     if os.path.isfile(args.path + ".index"):
-        saver.restore(sess, args.path)
+        agent.saver.restore(sess, args.path)
     else:
-        init.run()
-        copy_online_to_target.run()
+        agent.init.run()
+        agent.copy_online_to_target.run()
 
     writer = tf.summary.FileWriter("output", sess.graph)
 
     while True:
-        step = global_step.eval()
+        step = agent.global_step.eval()
         if step >= args.number_steps:
             break
         iteration += 1
@@ -177,21 +103,21 @@ with tf.Session() as sess:
             obs = env.reset()
             for skip in range(skip_start):  # skip the start of each game
                 obs, reward, done, info = env.step(0)
-            state = preprocess_observation(obs)
+            state = agent.preprocess_observation(obs)
 
         if args.render:
             env.render()
 
         # Online DQN evaluates what to do
-        q_values = online_q_values.eval(feed_dict={X_state: [state]})
-        action = epsilon_greedy(q_values, step)
+        q_values = agent.online_q_values.eval(feed_dict={X_state: [state]})
+        action = agent.epsilon_greedy(q_values, step)
 
         # Online DQN plays
         obs, reward, done, info = env.step(action)
-        next_state = preprocess_observation(obs)
+        next_state = agent.preprocess_observation(obs)
 
         # Let's memorize what happened
-        replay_memory.append((state, action, reward, next_state, 1.0 - done))
+        agent.replay_memory.append((state, action, reward, next_state, 1.0 - done))
         state = next_state
 
         if args.test:
@@ -208,23 +134,21 @@ with tf.Session() as sess:
         if iteration < training_start or iteration % args.learn_iterations != 0:
             continue  # only train after warmup period and at regular intervals
 
-        # Sample memories and use the target DQN to produce the target Q-Value
-        X_state_val, X_action_val, rewards, X_next_state_val, continues = (sample_memories(batch_size))
-        next_q_values = target_q_values.eval(feed_dict={X_state: X_next_state_val})
-        max_next_q_values = np.max(next_q_values, axis=1, keepdims=True)
-        y_val = rewards + continues * discount_rate * max_next_q_values
+        agent.sample()
 
         # Train the online DQN
-        _, loss_val, summary = sess.run([training_op, loss, merged],
-                               feed_dict={X_state: X_state_val, X_action: X_action_val, y: y_val})
+        _, loss_val, summary = sess.run([agent.training_op, agent.loss, agent.merged],
+                                        feed_dict={agent.X_state: agent.X_state_val,
+                                                   agent.X_action: agent.X_action_val,
+                                                   agent.y: agent.y_val})
 
         if step % 10 == 0:
             writer.add_summary(summary, step)
 
         # Regularly copy the online DQN to the target DQN
         if step % args.copy_steps == 0:
-            copy_online_to_target.run()
+            agent.copy_online_to_target.run()
 
         # And save regularly
         if step % args.save_steps == 0:
-            saver.save(sess, args.path)
+            agent.saver.save(sess, args.path)
